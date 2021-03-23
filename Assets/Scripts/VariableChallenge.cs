b6c694c991c65903830cf365a5fa8aa1ae798fd0
Rob Smith - smit293 <smit293@gmail.com>
2021-03-23 14:22:54 -0400

    FirstVariableChallengeComplete

---
+++ Assets/Scripts/VariableChallenge.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableChallenge : MonoBehaviour
+{
+    //create a var to store your name
+    //create a var to store your age
+    //create a var to store your speed
+    //create a health var
+    //create a score var
+    //var to check if all keys were collected
+    //var for ammo count
+
+    public string myName = "Rob";
+    public int myAge = 38;
+    public float speed = 5;
+    public int playerHealth = 100;
+    public int score = 0;
+    public int ammo = 5;
+    public bool keysGot = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My Name is: " + myName);
+        Debug.Log("My Age is: " + myAge);
+        Debug.Log("My Current Speed is: " + speed);
+        Debug.Log("My Health is: " + playerHealth);
+        Debug.Log("My Score is: " + score);
+        Debug.Log("Current Ammo is: " + ammo);
+        Debug.Log("Do I have the keys? " + keysGot);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
